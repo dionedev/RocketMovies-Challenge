f0c92e99f1c4dd9a7770852340205409d6c1699d
Dione Santos <dionedev@gmail.com>
2023-01-11 22:58:12 -0300

    feat: created controller and routes to list movie tags referring to user id

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express")
 const userRoutes = require("./user.routes")
 const movieNotesRoutes = require("./movieNotes.routes")
+const movieTagsRoutes = require("./movieTags.routes")
 
 const routes = Router()
 
 routes.use("/user", userRoutes)
 routes.use("/notes", movieNotesRoutes)
+routes.use("/tags", movieTagsRoutes)
 
 module.exports = routes
