d6c9670b0bb70a48bf6807f3438d2b0d23887385
Dione Santos <dionedev@gmail.com>
2023-01-18 23:12:39 -0300

    feat: added method to update user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -22,16 +22,48 @@ class UserController {
   }
 
   async showUserById(request, response) {
-    const { id } = request.params
+    const { user_id } = request.params
 
-    const user = await knex("users").where({ id })
+    const user = await knex("users").where({ id: user_id })
     
-    if(user.length != 1) {
+    if(!user.length) {
       throw new ErrorHandling("Não foi possível encontrar este usuário", 404)
     }
 
     response.status(200).json({ user })
   }
+
+  async updateUser(request, response) {
+    const { user_id } = request.params
+    const { updated_name, updated_email, current_password, updated_password } = request.body
+
+    const user = await knex("users").where({ id: user_id })
+    
+    if(!user.length) {
+      throw new ErrorHandling("Não foi possível encontrar este usuário", 404)
+    }
+
+    const userWithEmail = await knex('users').where({ email: updated_email }).first();
+
+    if(userWithEmail && userWithEmail.id !== user.id) {
+      throw new ErrorHandling("Este email não está disponível.")
+    }
+
+    await knex("users")
+    .update(
+      {
+        name: updated_name,
+        email: updated_email,
+        updated_at: new Date()
+      }
+    ).where({ id: user_id })
+
+    if(!current_password && updated_password) {
+      throw new ErrorHandling("Por favor, informe a senha atual para definir uma nova senha", 400)
+    }
+
+    response.status(200).json({ message: "Usuário atualizado com sucesso"})
+  } 
 
   async deleteUser(request, response) {
     const { user_id } = request.params
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,8 +3,9 @@ const UserController = require("../controllers/UserController")
 const userRoutes = Router()
 const userController = new UserController
 
-userRoutes.get("/:id", userController.showUserById)
+userRoutes.get("/:user_id", userController.showUserById)
 userRoutes.post("/", userController.createUser)
+userRoutes.put("/:user_id", userController.updateUser)
 userRoutes.delete("/:user_id", userController.deleteUser)
 
 module.exports = userRoutes
